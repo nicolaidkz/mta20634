ad3af0d41edbcfd09958f1bed20f7ec4cb693d12
exhex <c.kritik@gmail.com>
2020-05-04 20:51:08 +0200

    first communication of HS & Skater
    turns now based on HS, incorrect position calculated from targetPos dynamically.

--- BCIce Skater/Assets/Scripts/BasicGameMechanics/skaterMovement.cs
+++ BCIce Skater/Assets/Scripts/BasicGameMechanics/skaterMovement.cs
@@ -5,29 +5,32 @@ using UnityEngine;
 public class skaterMovement : MonoBehaviour
 {
     private GameObject targetPos;
+    GameObject incorrectPosMarker;
     public float speed = 1f;
     private int pointNr = 1;
     private bool correctInput = false; // To keep track of correct BCI input
-    private Vector2 correctPos;
+    private Vector3 incorrectPos;
     private bool collided = false;
     private bool posSet = false;
-    public float turnDistance = 5f;
+    public float turnDistance = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPos = GameObject.Find("Point"+pointNr); // Target Position Ice Skater will travel toward
-
+        targetPos = GameObject.Find("Point" + pointNr); // Target Position Ice Skater will travel toward
+        incorrectPos = targetPos.transform.position + ((targetPos.transform.position - transform.position).normalized * 2);
+        incorrectPosMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        incorrectPosMarker.transform.position = incorrectPos;
     }
-
     // Update is called once per frame
     void Update()
     {
+        
         //Debug.Log(correctPos);
-        if (Input.GetKeyDown("space"))
-        {
-            bciActivated(); // Run method for succesful BCI activation
-        }
+        // if (Input.GetKeyDown("space"))
+        //  {
+        //      bciActivated(); // Run method for succesful BCI activation
+        // }
 
         if (!collided)
         {
@@ -35,37 +38,51 @@ public class skaterMovement : MonoBehaviour
         }
         else
         {
-            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), correctPos, speed * Time.deltaTime); // Move Ice Skater toward correct position
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), incorrectPos, speed * Time.deltaTime); // Move Ice Skater toward correct position
         }
 
         if (Vector2.Distance(transform.position, targetPos.transform.position) < turnDistance && correctInput == true) // If distance between Ice Skater and Target Pos is less than x
         {
-            pointNr++; targetPos = GameObject.Find("Point"+pointNr); // Set next point as target pos
+            OnTargetPosChanged();
             correctInput = false;
             posSet = false;
             speed = 1f;
         }
 
-        if (Vector2.Distance(transform.position, correctPos) < 0.1f && collided == true) // If distance between Ice Skater and Target Pos is less than x
+        if (Vector2.Distance(transform.position, targetPos.transform.position) < turnDistance && correctInput == false) // If distance between Ice Skater and Target Pos is less than x
         {
-            pointNr++; targetPos = GameObject.Find("Point" + pointNr); // Set next point as target pos
-            correctInput = false;
-            collided = false;
-            posSet = false;
+            transform.position = Vector3.MoveTowards(transform.position,incorrectPos, speed * Time.deltaTime);
             speed = 1f;
         }
-
-        if (Vector2.Distance(transform.position, targetPos.transform.position) < turnDistance && !correctInput && !posSet)
+        if (Vector2.Distance(transform.position, incorrectPos) < turnDistance) 
         {
-            correctPos = transform.position; // Save correct turn position
-            posSet = true;
+            Vector3.Lerp(transform.position, targetPos.transform.position, 0.5f);
+            //correctInput = true;
         }
+
+        //if (Vector2.Distance(transform.position, incorrectPos) < 0.1f && collided == true) // If distance between Ice Skater and incorrect Pos is less than x
+        //{
+        //    pointNr++; targetPos = GameObject.Find("Point" + pointNr); // Set next point as target pos
+        //    correctInput = false;
+        //    collided = false;
+        //    posSet = false;
+        //    speed = 1f;
+        //}
+
+
     }
 
     void bciActivated()
     {
-        Debug.Log("Space Pressed");
+        Debug.Log("Direction Changed");
         correctInput = true;
+    }
+
+    void OnTargetPosChanged() 
+    {
+        pointNr++; targetPos = GameObject.Find("Point" + pointNr); // Set next point as target pos
+        incorrectPos = targetPos.transform.position + ((targetPos.transform.position - transform.position).normalized * 2); // arrange incorrectPos
+        incorrectPosMarker.transform.position = incorrectPos;
     }
 
     void OnTriggerEnter()
