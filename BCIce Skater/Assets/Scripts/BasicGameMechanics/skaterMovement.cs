14449f02a82773ef687d14b0a97b7f5a35f15bcf
Oliver <Oenger17@student.aau.dk>
2020-05-06 08:03:30 +0200

    Updated movement system
    Movement system works now. Without correct input, it hits wall and walks back to correct placement.

--- BCIce Skater/Assets/Scripts/BasicGameMechanics/skaterMovement.cs
+++ BCIce Skater/Assets/Scripts/BasicGameMechanics/skaterMovement.cs
@@ -6,70 +6,50 @@ public class skaterMovement : MonoBehaviour
 {
     private GameObject targetPos;
     GameObject incorrectPosMarker;
-    public float speed = 1f;
+    public float speed = 10f;
     private int pointNr = 1;
     private bool correctInput = false; // To keep track of correct BCI input
     private Vector3 incorrectPos;
     private bool collided = false;
-    private bool posSet = false;
-    public float turnDistance = 0.1f;
+    private float turnDistance = 0.1f;
+    private bool fail = false;
+    public float distancetoIncorrect = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         targetPos = GameObject.Find("Point" + pointNr); // Target Position Ice Skater will travel toward
-        incorrectPos = targetPos.transform.position + ((targetPos.transform.position - transform.position).normalized * 2);
+        incorrectPos = targetPos.transform.position + ((targetPos.transform.position - transform.position).normalized * distancetoIncorrect);
         incorrectPosMarker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         incorrectPosMarker.transform.position = incorrectPos;
     }
     // Update is called once per frame
     void Update()
     {
-        
-        //Debug.Log(correctPos);
-        // if (Input.GetKeyDown("space"))
-        //  {
-        //      bciActivated(); // Run method for succesful BCI activation
-        // }
 
-        if (!collided)
+        if (Input.GetKeyDown("space"))
         {
-        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), targetPos.transform.position, speed * Time.deltaTime); // Move Ice Skater toward target position
+            bciActivated(); // Run method for succesful BCI activation
         }
-        else
+        if (Vector2.Distance(transform.position, targetPos.transform.position) > turnDistance && !fail)
         {
-            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), incorrectPos, speed * Time.deltaTime); // Move Ice Skater toward correct position
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), targetPos.transform.position, speed * Time.deltaTime); // Move Ice Skater toward target position
         }
-
-        if (Vector2.Distance(transform.position, targetPos.transform.position) < turnDistance && correctInput == true) // If distance between Ice Skater and Target Pos is less than x
+        else if (Vector2.Distance(transform.position, targetPos.transform.position) < turnDistance && correctInput)
         {
             OnTargetPosChanged();
-            correctInput = false;
-            posSet = false;
-            speed = 1f;
         }
-
-        if (Vector2.Distance(transform.position, targetPos.transform.position) < turnDistance && correctInput == false) // If distance between Ice Skater and Target Pos is less than x
+        else if (!correctInput && !collided)
         {
-            transform.position = Vector3.MoveTowards(transform.position,incorrectPos, speed * Time.deltaTime);
-            speed = 1f;
+            fail = true;
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), incorrectPos, speed * Time.deltaTime);
         }
-        if (Vector2.Distance(transform.position, incorrectPos) < turnDistance) 
+        if ((Vector2.Distance(transform.position, incorrectPos) < turnDistance && fail) || collided)
         {
-            Vector3.Lerp(transform.position, targetPos.transform.position, 0.5f);
-            //correctInput = true;
+            collided = true;
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), targetPos.transform.position, speed * Time.deltaTime); // Move Ice Skater toward target position
+            correctInput = true;
         }
-
-        //if (Vector2.Distance(transform.position, incorrectPos) < 0.1f && collided == true) // If distance between Ice Skater and incorrect Pos is less than x
-        //{
-        //    pointNr++; targetPos = GameObject.Find("Point" + pointNr); // Set next point as target pos
-        //    correctInput = false;
-        //    collided = false;
-        //    posSet = false;
-        //    speed = 1f;
-        //}
-
-
     }
 
     void bciActivated()
@@ -80,15 +60,11 @@ public class skaterMovement : MonoBehaviour
 
     void OnTargetPosChanged() 
     {
+        collided = false;
+        fail = false;
+        correctInput = false;
         pointNr++; targetPos = GameObject.Find("Point" + pointNr); // Set next point as target pos
-        incorrectPos = targetPos.transform.position + ((targetPos.transform.position - transform.position).normalized * 2); // arrange incorrectPos
+        incorrectPos = targetPos.transform.position + ((targetPos.transform.position - transform.position).normalized * distancetoIncorrect); // arrange incorrectPos
         incorrectPosMarker.transform.position = incorrectPos;
-    }
-
-    void OnTriggerEnter()
-    {
-        Debug.Log("test");
-        collided = true;
-        speed = 0.5f;
     }
 }
