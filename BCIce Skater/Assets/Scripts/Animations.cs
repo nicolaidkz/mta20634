c7737f741f0fb0e21bdad660d4ee5d837cd06c46
Unknown <ndanie16@student.aau.dk>
2020-05-12 14:41:48 +0200

    finished animations ish

--- BCIce Skater/Assets/Scripts/Animations.cs
+++ BCIce Skater/Assets/Scripts/Animations.cs
@@ -6,38 +6,78 @@ using UnityEngine;
 public class Animations : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private bool flip;
+    public static bool isRunning = false;
+    public static bool rotate = false;
+    private float elapsedTime = 0.0f;
+    //private Vector3 startPosition;
+    //private Quaternion startRotation = transform.rotation;
+    private Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        flip = true;
+        targetRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, -45.0f));
     }
 
     IEnumerator FlipAnim()
     {
-        if(spriteRenderer.flipX == true)
+        while (isRunning)
         {
-            spriteRenderer.flipX = false;
-        }
-        if(spriteRenderer.flipX == false)
-        {
-            spriteRenderer.flipX = true;
-        }
+            if (spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
 
-        yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.7f);
+        } 
     }
 
-    public void OnGameStateChanged(GameData gameData)
+    IEnumerator RotateAnim(float time)
     {
-        while(gameData.gameState == GameState.Running)
+        if (!rotate)
         {
-            StartCoroutine(FlipAnim());
+            Quaternion startRotation = transform.rotation;
+            float originalTime = time;
+            while (time > 0.0f)
+            {
+                time -= Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(startRotation, Quaternion.Euler(0, 0, -45), 1 - (time / originalTime));
+            }
         }
+        if (rotate)
+        {
+            Quaternion startRotation = transform.rotation;
+            float originalTime = time;
+            while (time > 0.0f)
+            {
+                time -= Time.deltaTime;
+                transform.rotation = Quaternion.Lerp(startRotation, Quaternion.Euler(0, 0, 45), 1 - (time / originalTime));
+                
+            }
+        }
+
+        yield return new WaitForSeconds(0.7f);
+    }
+
+
+
+    public void ToggleIsRunning()
+    {
+        StopCoroutine(FlipAnim());
+        isRunning = !isRunning;
+        StartCoroutine(FlipAnim());
+    }
+
+    public void ToggleRotate()
+    {
+        StopCoroutine(RotateAnim(7.0f));
+        StartCoroutine(RotateAnim(7.0f));
     }
 }
